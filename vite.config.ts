d81becdb74352bfb7e0cdf2faf0337d43d647c3f
Hussain <hkosaimi@gmail.com>
2026-02-15 05:53:44 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  base: "/",
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));
